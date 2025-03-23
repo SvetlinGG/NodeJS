3508afac10ba80a29dc938788f6d7359479d4c38
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-24 00:51:44 +0100

    modules working

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const sum = require('./calculator');
+const executeCalc = require('./third-party-modules');
 
 console.log(sum(4, 5));
+executeCalc();
---
+++ third-party-modules.js
@@ -0,0 +1,11 @@
+
+const calculator = require('calculator');
+
+function execute(){
+    var f = calculator.func('f(x) = x*10 - 20')
+     console.log(f(3));
+       
+
+
+}
+module.exports = execute;
